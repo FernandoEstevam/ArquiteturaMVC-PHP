a24c90f2bfbb04300b4184664f9c0737bfd0c0ce
Fernando Estevam <fernandoestevam23@gmail.com>
2018-10-23 01:10:12 -0300

    Acrescentando rotas com parametros

--- app/routes.php
+++ app/routes.php
@@ -4,10 +4,12 @@
 
 /** 
  * 1° rota => - quando não digitar nada depois da '/' => 'chama' HomeController@index
- * 
+ * 2º rota => - quando o usuario passa um parametro '/posts' => 'chama' PostsController@show
  * 
  * **/
 
 $routes[] = ['/', 'HomeController@index'];
+$routes[] = ['/posts', 'PostsController@index'];
+$routes[] = ['/post/{id}/show', 'PostsController@show'];
 
 return $routes;
