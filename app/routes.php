042fed3176ed79f750b6d83ad97c924c0208e3d0
Fernando Estevam <fernandoestevam23@gmail.com>
2018-10-22 22:32:11 -0300

    Adicionando arquivos de routas 'routes.php'

---
+++ app/routes.php
@@ -0,0 +1,13 @@
+<?php 
+
+/** Criando Definição de routas da aplicação **/
+
+/** 
+ * 1° rota => - quando não digitar nada depois da '/' => 'chama' HomeController@index
+ * 
+ * 
+ * **/
+
+$routes[] = ['/','HomeController@index'];
+
+return $routes;
