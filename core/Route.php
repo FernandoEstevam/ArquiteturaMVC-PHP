50528510d85c4c7c16082bcb2be2c132e06a405e
Fernando Estevam <fernandoestevam23@gmail.com>
2018-10-23 01:12:11 -0300

    Fazendo a comparação de url com parametros no metodo run()

--- core/Route.php
+++ core/Route.php
@@ -4,6 +4,7 @@
  *  parse_url() captura o endereço na url e transforma em uma array
  *  PHP_URL_PATH seta o indice do array path/ "http://php.net/manual/pt_BR/function.parse-url.php"
  * 
+ * Método run() faz a comparação da url verifica se existe algum parametro e faz a troca dentro da {}
  * **/
 
 namespace Core;
@@ -28,7 +29,28 @@ class Route
     //Comparando URL
     private function run()
     {   
-        echo $this->getUrl();
+        $url = $this->getUrl();
+        $urlArray = explode('/', $url);
+        print_r($urlArray);
+        echo "<br/>";
+
+        foreach($this->routes as $routes){
+            $routeArray = explode('/', $routes[0]);
+            echo "<br/>";
+            print_r($routeArray);
+
+            for($i = 0; $i < count($routeArray); $i++){
+                if((strpos($routeArray[$i], "{") !== false) && (count($urlArray) == count($routeArray))){
+                    $routeArray[$i] = $urlArray[$i];
+                }
+                $route[0] = implode($routeArray, '/');
+            }
+            if($url == $route[0]){
+                echo "<br/>Rota valida";
+            } else {
+                echo "<br/>Rota invalida";
+            }
+        }
     }
 
 }
