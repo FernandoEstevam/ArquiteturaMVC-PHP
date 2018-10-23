2f9c9b944e222b362fd44f4ddb81a7d0506e8f08
Fernando Estevam <fernandoestevam23@gmail.com>
2018-10-22 22:16:49 -0300

    Inclui a classe Bootstrap

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 //Carrega nossa aplicação
 
-//1º recarrega o autoload
+//1º recarrega o autoload de todas as classes
 require_once __DIR__."/../vendor/autoload.php";
+
+//2º Importa o arquivo Bootstrap onde tudo precisa ser inicializado
+require_once __DIR__."/../core/Bootstrap.php";
