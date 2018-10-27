a9caad932545aea38d057973d0addb1085250d3a
Fernando Estevam <fernandoestevam23@gmail.com>
2018-10-26 23:48:06 -0300

    Criando um novo metodo pageNotFound que chama a página de erro 404.phtml

--- core/Container.php
+++ core/Container.php
@@ -12,4 +12,13 @@ class Container
         //Cria um novo objeto a partir do controller de rotas
         return new $objController;
     }
+
+    //Metodo pagina de erro verifica se o arquivo existe, caso sim chama o arquivo
+    public static function pageNotFound(){
+        if(file_exists('../App/Views/Erro/404.phtml')){
+            return  require_once "../App/Views/Erro/404.phtml";
+        } else {
+            echo "Error 404: Page not found!";
+        }
+    }
 }
